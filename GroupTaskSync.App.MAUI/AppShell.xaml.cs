32a4f1fc1496e4328cd74ecda01435e7fbadcb94
Pawel Podzorski <pawelpodzorski1@gmail.com>
2025-09-05 12:04:13 +0200

    Add project files.

---
+++ GroupTaskSync.App.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GroupTaskSync.App.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GroupTaskSync.App.MAUI/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "GroupTaskSync.App.MAUI")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "GroupTaskSync.App.MAUI.Pages")]
